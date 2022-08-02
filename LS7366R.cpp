7013f5bc67203859f984a1fadaa805f7873d8934
Tyler W <tylerdwerner@gmail.com>
2022-08-02 12:27:24 -0400

    Removed useless comment

--- LS7366R.cpp
+++ LS7366R.cpp
@@ -10,47 +10,6 @@ void LS7366R::initEncoder() {
   SPI.transfer(0x03);   // Write to MDR1
   digitalWrite(ssPort, 1); // End Communication
 
-  /*
-  MDR0:
-      B1 B0 = 00: non-quadrature count mode. (A = clock, B = direction).
-            = 01: x1 quadrature count mode (one count per quadrature cycle).
-            = 10: x2 quadrature count mode (two counts per quadrature cycle).
-            = 11: x4 quadrature count mode (four counts per quadrature cycle).
-      B3 B2 = 00: free-running count mode.
-            = 01: single-cycle count mode (counter disabled with carry or borrow, re-enabled with reset or load).
-            = 10: range-limit count mode (up and down count-ranges are limited between DTR and zero,
-                    respectively; counting freezes at these limits but resumes when direction reverses).
-            = 11: modulo-n count mode (input count clock frequency is divided by a factor of (n+1),
-                    where n = DTR, in both up and down directions).
-      B5 B4 = 00: disable index.
-            = 01: configure index as the "load CNTR" input (transfers DTR to CNTR).
-            = 10: configure index as the "reset CNTR" input (clears CNTR to 0).
-            = 11: configure index as the "load OTR" input (transfers CNTR to OTR).
-      B6 = 0: Asynchronous Index
-         = 1: Synchronous Index (overridden in non-quadrature mode)
-      B7 = 0: Filter clock division factor = 1
-         = 1: Filter clock division factor = 2
-  
-  MDR1:
-      B1 B0 = 00: 4-byte counter mode
-            = 01: 3-byte counter mode
-            = 10: 2-byte counter mode.
-            = 11: 1-byte counter mode
-      B2 = 0: Enable counting
-         = 1: Disable counting
-      B3 = : not used
-      B4 = 0: NOP
-         = 1: FLAG on IDX (B4 of STR)
-      B5 = 0: NOP
-         = 1: FLAG on CMP (B5 of STR)
-      B6 = 0: NOP
-         = 1: FLAG on BW (B6 of STR)
-      B7 = 0: NOP
-         = 1: FLAG on CY (B7 of STR
-
-  */
-  
-
 }
 
 void LS7366R::initEncoderDev(uint8_t MDR0, uint8_t MDR1) {
